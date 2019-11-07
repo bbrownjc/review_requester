a482fbd6b7cce0e01344eb9f965a93b369dcdafe
mmontanob <michael.montano@jumpcloud.com>
2019-11-07 11:21:43 -0600

    add documentation and params to API endpoints

--- app.py
+++ app.py
@@ -22,48 +22,71 @@ class ReviewRequest(db.Model):
     pass
 
 
-reviewers = api.namespace("reviewers", description="Reviewer Management")
-
 #################
 # API Endpoints #
 #################
 
+reviewers = api.namespace("reviewers", description="Reviewer Management")
+# TODO: Add public fields
+_reviewer =  api.model('reviewer', {})
 
 @reviewers.route("/")
 class ReviewerList(Resource):
+
+    @reviewers.doc('list of registered reviewers')
+    @reviewers.marshal_list_with(_reviewer, envelope='data')
     def get(self):
         """List of reviewers."""
         pass
 
+    @reviewers.response(201, 'Reviewer successfully created.')
+    @reviewers.doc('create a new reviewer')
+    @reviewers.expect(_reviewer, validate=True)
     def post(self):
         """Add a reviewer."""
         pass
 
 
 @reviewers.route("/<int:id>")
+@reviewers.param('id', 'The Reviewer identifier')
+@reviewers.response(404, 'Reviewer not found.')
 class ReviewerManagment(Resource):
-    def get(self):
+
+    @reviewers.doc('get a reviewer')
+    @reviewers.marshal_with(_reviewer)
+    def get(self, id):
         """Retrieve a single reviewer."""
         pass
 
-    def put(self):
+    @reviewers.response(200, 'Reviewer successfully updated.')
+    @reviewers.doc('updates a reviewer')
+    @reviewers.expect(_reviewer, validate=True)
+    def put(self, id):
         """Update a reviewer."""
         pass
 
-    def delete(self):
+    @reviewers.doc('removes a reviewer')
+    def delete(self, id):
         """Remove a reviewer."""
         pass
 
 
 reviews = api.namespace("reviews", description="Review Request Data")
-
+# TODO: Add public fields
+_review = api.model('review', {})
 
 @reviews.route("/")
 class Reviews(Resource):
+
+    @reviews.doc('list of registered reviews')
+    @reviews.marshal_list_with(_review, envelope='data')
     def get(self):
         """List of reviews."""
         pass
 
+    @reviews.response(201, 'Review successfully created.')
+    @reviews.doc('create a new review')
+    @reviews.expect(_review, validate=True)
     def post(self):
         """Add a review."""
         pass
